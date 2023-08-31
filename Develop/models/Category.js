4c7e2a54aef9c8acd4a396e1b3be575646edb27c
Tova Greene <tovalgreene@gmail.com>
2023-08-31 12:01:25 -0400

    Edited README and defined columns in category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -4,11 +4,26 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+ // define columns: id --
+    //Integer, Doesn't allow null values, 
+    // Set as primary key, Uses auto increment
+    // category_name -- 
+    // String, Doesn't allow null values
+
 Category.init(
   {
-    // define columns
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   },
   },
-  {
+    {
     sequelize,
     timestamps: false,
     freezeTableName: true,
