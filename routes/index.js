74adbcc8f13c54ff483f040d0603756b30dab97f
Tova Greene <tovalgreene@gmail.com>
2023-11-13 23:46:52 -0500

    edited index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// API Routes
 router.use('/api', apiRoutes);
 
+// If no API routes are hit, send a 404 error response
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  // You could log the wrong route access here
+  console.error(`404 Error: The route ${req.originalUrl} does not exist.`);
+
+  // Sending a 404 status code and a JSON response
+  res.status(404).json({
+    message: "Sorry, we couldn't find the page you were looking for."
+  });
 });
 
 module.exports = router;
